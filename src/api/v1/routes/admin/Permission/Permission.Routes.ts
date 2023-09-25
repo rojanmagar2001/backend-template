ea64c7f42a7da8e6e7df02dfd4a0e21fc24265a5
Rojan Rana Magar <rojan.magar@aitc.ai>
2023-09-25 12:09:33 +0545

    changes

--- src/api/v1/routes/admin/Permission/Permission.Routes.ts
+++ src/api/v1/routes/admin/Permission/Permission.Routes.ts
@@ -1,11 +1,12 @@
 import AdminPermissionController from '@/api/v1/controllers/admin/Permission/Admin.Permission';
 import { PermissionCreateSchema } from '@/api/v1/validators/admin/Permission/Permission.Validator';
+import { hasAccessRights } from '@/middleware/auth.middleware';
 import validate from '@/middleware/validation.middleware';
 import { Router } from 'express';
 
 const router = Router();
 
-router.post('/permission', validate(PermissionCreateSchema), AdminPermissionController.create);
+router.post('/permission', hasAccessRights, validate(PermissionCreateSchema), AdminPermissionController.create);
 
 const AdminPermissionRoutes = router;
 
--- src/api/v1/routes/admin/Role/Role.Routes.ts
+++ src/api/v1/routes/admin/Role/Role.Routes.ts
@@ -1,13 +1,14 @@
 import AdminRoleController from '@/api/v1/controllers/admin/Role/Admin.Role';
 import { RoleCreateSchema } from '@/api/v1/validators/admin/Role/Role.Validator';
+import { hasAccessRights } from '@/middleware/auth.middleware';
 import validate from '@/middleware/validation.middleware';
 import { Router } from 'express';
 
 const router = Router();
 
-router.post('/role', validate(RoleCreateSchema), AdminRoleController.create);
+router.post('/role', hasAccessRights, validate(RoleCreateSchema), AdminRoleController.create);
 
-router.get('/role/:id', AdminRoleController.getSingle);
+router.get('/role/:id', hasAccessRights, AdminRoleController.getSingle);
 
 const AdminRoleRoutes = router;
 
